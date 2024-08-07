4aca40d794d62f109080457e2814ef9a3303dca9
alia178 <ahmedabdulkadirali@gmail.com>
2024-08-07 13:13:27 +0100

    add bashscript

---
+++ firstbash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "hello world!"
+pwd
+ls
+echo -n "Enter Any Number: "
+read -r n
+i=1; c=1
+while [ $i -le "$n" ]; do
+	i=$((i + 1))
+	r=$((n % i))
+	[ $r -eq 0 ] && c=$((c + 1))
+done
+
+if [ $c -eq 2 ]; then
+	echo "Prime"
+else
+	echo "Not Prime"
+fi
+
